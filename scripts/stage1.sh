be49178c40faf1e9a123486c05775fc6e3fb7d43
Kirill Kuznetsov <kirill.cuznetsov2002@yandex.ru>
2023-05-07 19:53:06 +0300

    first stage

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -1,7 +1,22 @@
 #!/bin/bash
 
+psql -U postgres -f ../sql/create_database.sql
+
+echo "Database created successfully."
+
 psql -d project -U postgres -f ../sql/create_table.sql
 
 echo "Table created successfully."
 
 psql -d project -U postgres -f ../sql/load_csv.sql
+
+echo "Data from csv loaded successfully."
+
+sqoop import-all-tables -Dmapreduce.job.user.classpath.first=true \
+ -Dorg.apache.sqoop.splitter.allow_text_splitter=true \
+ --connect jdbc:postgresql://localhost/project \
+ --username postgres --warehouse-dir /project \
+ --as-avrodatafile --compression-codec=snappy \
+ --outdir /project/avsc --m 1
+
+echo "Data from csv PG to HDFS imported successfully."
