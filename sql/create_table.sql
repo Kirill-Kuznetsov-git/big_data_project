37e4412d7b67bcc9f7198bde7d441acc98cc9dd0
Kirill Kuznetsov <kirill.cuznetsov2002@yandex.ru>
2023-05-07 17:07:25 +0300

    fix

--- sql/create_table.sql
+++ sql/create_table.sql
@@ -4,7 +4,7 @@ CREATE TABLE IF NOT EXISTS trips (
     trip_id VARCHAR(120) PRIMARY KEY,
     call_type VARCHAR(10) NOT NULL,
     origin_call INTEGER NULL,
-    origin_stand INTEGER NULL,
+    origin_stand DOUBLE NULL,
     taxi_id INTEGER NOT NULL,
     timestamp BIGINT NOT NULL,
     day_type VARCHAR(10) NOT NULL,
