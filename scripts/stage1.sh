6bf9450db617a66cae75ed5e0e28d4acd7560677
Kirill Kuznetsov <kirill.cuznetsov2002@yandex.ru>
2023-05-08 22:43:19 +0300

    fix

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -21,6 +21,6 @@ sqoop import-all-tables -Dmapreduce.job.user.classpath.first=true \
  --connect jdbc:postgresql://localhost/project \
  --username postgres --warehouse-dir /project \
  --as-avrodatafile --compression-codec=snappy \
- --outdir /project/avsc --m 1
+ --outdir /project --m 1
 
 echo "Data from csv PG to HDFS imported successfully."
--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-hdfs dfs -put /project/avsc/trips.avsc /project/trips.avsc
+hdfs dfs -put /project/trips.avsc /project/trips.avsc
 
 echo "Avsc file imported from local machine to hdfs successfully."
 
