7ec4fd8fcd9b4d9cf3e0ee1e08e0837aba8c05eb
YY7 <yy7@pop-os.localdomain>
2023-05-11 02:10:05 +0300

    add saving gbt preditions saving to csv

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -11,6 +11,7 @@ rm -rf output/evaluation*
 rm -rf output/pipeline_output*
 rm -rf output/lr*
 rm -rf output/rf*
+rm -rf output/gbt*
 
 # python3 scripts/EDA.py
 spark-submit --jars /usr/hdp/current/hive-client/lib/hive-metastore-1.2.1000.2.6.5.0-292.jar,/usr/hdp/current/hive-client/lib/hive-exec-1.2.1000.2.6.5.0-292.jar --packages org.apache.spark:spark-avro_2.12:3.0.3 scripts/EDA.py
