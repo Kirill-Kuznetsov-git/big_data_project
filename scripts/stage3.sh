fee4aff1e8cb097be774b80bba3ac71d31517743
YY7 <yy7@pop-os.localdomain>
2023-05-10 14:32:52 +0300

    use spark submit

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -8,6 +8,8 @@ rm -rf output/q5
 rm -rf output/q6
 rm -rf output/q7
 
-python3 scripts/EDA.py
+# python3 scripts/EDA.py
+spark-submit --jars /usr/hdp/current/hive-client/lib/hive-metastore-1.2.1000.2.6.5.0-292.jar,/usr/hdp/current/hive-client/lib/hive-exec-1.2.1000.2.6.5.0-292.jar --packages org.apache.spark:spark-avro_2.12:3.0.3 scripts/EDA.py
 
-python3 scripts/PDA.py
+# python3 scripts/PDA.py
+spark-submit --jars /usr/hdp/current/hive-client/lib/hive-metastore-1.2.1000.2.6.5.0-292.jar,/usr/hdp/current/hive-client/lib/hive-exec-1.2.1000.2.6.5.0-292.jar --packages org.apache.spark:spark-avro_2.12:3.0.3 scripts/PDA.py
