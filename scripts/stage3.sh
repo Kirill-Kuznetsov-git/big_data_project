e28a3a54bbdf9c017c5e4c0a128bb99ba733598a
Kirill Kuznetsov <kirill.cuznetsov2002@yandex.ru>
2023-05-11 00:24:15 +0300

    uncomment EDA

--- scripts/stage3.sh
+++ scripts/stage3.sh
@@ -13,7 +13,7 @@ rm -rf output/lr*
 rm -rf output/rf*
 
 # python3 scripts/EDA.py
-# spark-submit --jars /usr/hdp/current/hive-client/lib/hive-metastore-1.2.1000.2.6.5.0-292.jar,/usr/hdp/current/hive-client/lib/hive-exec-1.2.1000.2.6.5.0-292.jar --packages org.apache.spark:spark-avro_2.12:3.0.3 scripts/EDA.py
+spark-submit --jars /usr/hdp/current/hive-client/lib/hive-metastore-1.2.1000.2.6.5.0-292.jar,/usr/hdp/current/hive-client/lib/hive-exec-1.2.1000.2.6.5.0-292.jar --packages org.apache.spark:spark-avro_2.12:3.0.3 scripts/EDA.py
 
 # python3 scripts/PDA.py
 spark-submit --jars /usr/hdp/current/hive-client/lib/hive-metastore-1.2.1000.2.6.5.0-292.jar,/usr/hdp/current/hive-client/lib/hive-exec-1.2.1000.2.6.5.0-292.jar --packages org.apache.spark:spark-avro_2.12:3.0.3 scripts/PDA.py
