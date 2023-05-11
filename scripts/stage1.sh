267454bbac58c350dc9d1c232c6f4910062a9945
Kirill Kuznetsov <kirill.cuznetsov2002@yandex.ru>
2023-05-11 04:37:10 +0300

    fix

--- scripts/stage1.sh
+++ scripts/stage1.sh
@@ -2,23 +2,24 @@
 
 wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1xlhxseaFSlpI6MTfa7ks91CG3RKrzmUQ' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1xlhxseaFSlpI6MTfa7ks91CG3RKrzmUQ" -O data/trips.tsv && rm -rf /tmp/cookies.txt
 
-wget https://jdbc.postgresql.org/download/postgresql-42.6.0.jar --no-check-certificate
+#wget https://jdbc.postgresql.org/download/postgresql-42.6.0.jar --no-check-certificate
 
-cp  postgresql-42.6.0.jar /usr/hdp/current/sqoop-client/lib/
+#cp  postgresql-42.6.0.jar /usr/hdp/current/sqoop-client/lib/
 
-psql -U postgres -f sql/create_database.sql
-
-echo "Database created successfully."
-
-psql -d project -U postgres -f sql/create_table.sql
-
-echo "Table created successfully."
-
-psql -d project -U postgres -f sql/load_csv.sql
+#psql -U postgres -f sql/create_database.sql
+#
+#echo "Database created successfully."
+#
+#psql -d project -U postgres -f sql/create_table.sql
+#
+#echo "Table created successfully."
+#
+#psql -d project -U postgres -f sql/load_csv.sql
 
 echo "Data from csv loaded successfully."
 
 hdfs dfs -rm -r /project
+rm /project/trips.avsc
 
 sqoop import-all-tables -Dmapreduce.job.user.classpath.first=true \
  -Dorg.apache.sqoop.splitter.allow_text_splitter=true \
