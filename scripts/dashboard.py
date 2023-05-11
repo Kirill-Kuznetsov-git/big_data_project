989142fa9567067dd47ed70e4e02d83249d391a5
YY7 <yy7@pop-os.localdomain>
2023-05-11 08:50:10 +0300

    fill dashboard

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -3,20 +3,20 @@ import streamlit as st
 import pandas as pd
 
 trips = pd.read_csv("data/trips.tsv", sep="\t")
-trips_prproc = pd.read_csv("output/trips_preprocessed.tsv", sep="\t")
+# trips_prproc = pd.read_csv("output/trips_preprocessed.tsv", sep="\t")
 
-q1 = pd.read_csv("output/q1.csv")
-q2 = pd.read_csv("output/q2.csv")
-q3 = pd.read_csv("output/q3.csv")
-q4 = pd.read_csv("output/q4.csv")
-q5 = pd.read_csv("output/q5.csv")
-q6 = pd.read_csv("output/q6.csv")
-q7 = pd.read_csv("output/q7.csv")
+# q1 = pd.read_csv("output/q1.csv")
+# q2 = pd.read_csv("output/q2.csv")
+# q3 = pd.read_csv("output/q3.csv")
+# q4 = pd.read_csv("output/q4.csv")
+# q5 = pd.read_csv("output/q5.csv")
+# q6 = pd.read_csv("output/q6.csv")
+# q7 = pd.read_csv("output/q7.csv")
 
-evals = pd.read_csv("output/evaluations.csv")
-lr = pd.read_csv("output/lr.csv")
-rf = pd.read_csv("output/rf.csv")
-gbt = pd.read_csv("output/gbt.csv")
+# evals = pd.read_csv("output/evaluations.csv")
+# lr = pd.read_csv("output/lr.csv")
+# rf = pd.read_csv("output/rf.csv")
+# gbt = pd.read_csv("output/gbt.csv")
 
 
 # TODO: add project name
@@ -29,17 +29,17 @@ st.write(trip_description)
 
 
 # hour by call type by trip time sec
-import altair as alt
-c = alt.Chart(trips_prproc).mark_circle().encode(
-    x='hour', y='call_type', size='trip_time_sec', color='sal', tooltip=['ename', 'deptno', 'trip_time_sec'])
-st.write(c)
+# import altair as alt
+# c = alt.Chart(trips_prproc).mark_circle().encode(
+#     x='hour', y='call_type', size='trip_time_sec', color='sal', tooltip=['ename', 'deptno', 'trip_time_sec'])
+# st.write(c)
 
 
 
 # q1 - Missing values
 
-trip_description = trips.describe()
-st.write(trip_description)
+# trip_description = trips.describe()
+# st.write(trip_description)
 
 # q2 - Day of week
 
