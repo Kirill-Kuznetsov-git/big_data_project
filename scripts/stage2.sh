afd64c0f5e7732a658dce2d121b13b12399dcb2e
Kirill Kuznetsov <kirill.cuznetsov2002@yandex.ru>
2023-05-11 20:33:53 +0300

    fix

--- scripts/stage2.sh
+++ scripts/stage2.sh
@@ -13,8 +13,8 @@ rm -rf output/q6*
 rm -rf output/q7*
 
 
-# python scripts/eda.py
-spark-submit --jars /usr/hdp/current/hive-client/lib/hive-metastore-1.2.1000.2.6.5.0-292.jar,/usr/hdp/current/hive-client/lib/hive-exec-1.2.1000.2.6.5.0-292.jar --packages org.apache.spark:spark-avro_2.12:3.0.3 scripts/eda.py
+# python scripts/eda_file.py
+spark-submit --jars /usr/hdp/current/hive-client/lib/hive-metastore-1.2.1000.2.6.5.0-292.jar,/usr/hdp/current/hive-client/lib/hive-exec-1.2.1000.2.6.5.0-292.jar --packages org.apache.spark:spark-avro_2.12:3.0.3 scripts/eda_file.py
 
 
 # merge csv files into one
