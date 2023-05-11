c76fcbcc168a1eef213026b3edaad2b6b82cd0e2
Kirill Kuznetsov <kirill.cuznetsov2002@yandex.ru>
2023-05-11 13:44:25 +0300

    fix

--- scripts/dashboard.py
+++ scripts/dashboard.py
@@ -5,28 +5,25 @@ import pandas as pd
 # trips = pd.read_csv("data/trips.tsv", sep="\t")
 trips_prproc = pd.read_csv("output/trips_preprocessed.csv")
 
-# q1 = pd.read_csv("output/q1.csv")
+q1 = pd.read_csv("output/q1.csv")
 q2 = pd.read_csv("output/q2.csv")
-# q3 = pd.read_csv("output/q3.csv")
-# q4 = pd.read_csv("output/q4.csv")
-# q5 = pd.read_csv("output/q5.csv")
-# q6 = pd.read_csv("output/q6.csv")
-# q7 = pd.read_csv("output/q7.csv")
+q3 = pd.read_csv("output/q3.csv")
+q4 = pd.read_csv("output/q4.csv")
+q5 = pd.read_csv("output/q5.csv")
+q6 = pd.read_csv("output/q6.csv")
+q7 = pd.read_csv("output/q7.csv")
 
-# evals = pd.read_csv("output/evaluations.csv")
-# lr = pd.read_csv("output/lr.csv")
-# rf = pd.read_csv("output/rf.csv")
-# gbt = pd.read_csv("output/gbt.csv")
+evals = pd.read_csv("output/evaluations.csv")
+lr = pd.read_csv("output/lr.csv")
+rf = pd.read_csv("output/rf.csv")
 
 
-# TODO: add project name
 st.write("# Big Data Project  \n _Estimated travel time for a taxi ride_  \n", "*Year*: **2023**")
-
+st.write("")
 
 # # Display the descriptive information of the dataframe
 # trip_description = trips.describe()
 # st.write(trip_description)
-
 
 # hour by call type by trip time sec
 import altair as alt
@@ -53,7 +50,7 @@ q2['avg_trip_time'] = q2['avg_trip_time'].astype('int')
 # countplot / horizontal
 chart = alt.Chart(q2).mark_bar().encode(
     x="day_of_week:O",
-    y="avg_trip_time:Q").properties(height=700)
+    y="avg_trip_time:Q").properties(height=300, weight=00)
 
 st.write(chart)
 
